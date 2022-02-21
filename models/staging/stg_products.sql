02f89f3c1f75f56998842cbfbda0dd79e82c7ae4
gabrielbonin01 <78220230+gabrielbonin01@users.noreply.github.com>
2022-02-21 14:10:53 -0500

    Revomed issue with Id as a string from supplier_ids

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -1,5 +1,25 @@
 with source_datalake as (
-    select * from {{ source('datalake', 'products') }}
+        select cast(trim(supplier_ids, '"')as integer) as supplier_ids,
+        id,
+        product_code,
+        product_name,
+        description,
+        standard_cost,
+        list_price,
+        reorder_level,
+        target_level,
+        quantity_per_unit,
+        discontinued,
+        minimum_reorder_quantity,
+        category,
+        attachments
+    from {{ source('datalake', 'products') }}    
+    where supplier_ids not like('%;%')
+    
 )
 
 select *,current_timestamp() as ingestion_timestamp from source_datalake
+
+
+
+
